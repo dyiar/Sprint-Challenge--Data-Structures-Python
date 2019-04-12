f8683a0b68e2be5565f35479c19a7401e9f43e77
dyiar <austin.hl.blake@gmail.com>
2019-04-12 10:56:28 -0600

    finished sorting names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from heap import sorted
 
 start_time = time.time()
 
@@ -10,11 +11,29 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+sorted1 = sorted(names_1)
+sorted2 = sorted(names_2)
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+i=0
+j=0
+
+while i < len(names_1) and j<len(names_2):
+    if sorted1[i] == sorted2[j]:
+        duplicates.append(sorted1[i])
+        i+=1
+        j+=1
+    elif sorted1[i] < sorted2[j]:
+        j+=1
+    else:
+        i+=1
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
